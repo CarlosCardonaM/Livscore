17adc8d9de526d1a5075202d7df63f0e6efcf16e
Carlos Cardona <cardonacarlos97.06@gmail.com>
2022-04-28 00:20:50 -0500

    Add Onboarding
    Add Onboarding group and  ChooseTeamViewController.swift and set it as initial ViewController in Storyboard

---
+++ Livscore/Controllers/Onboarding/ChooseTeamViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseTeamViewController.swift
+//  Livscore
+//
+//  Created by Carlos Cardona on 28/04/22.
+//
+
+import UIKit
+
+class ChooseTeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
